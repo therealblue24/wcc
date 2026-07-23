e37322556d949f851dc3a3510f0cc53ad56b426d
therealblue24 <99767624+therealblue24@users.noreply.github.com>
2026-07-23 10:40:26 -0400

    struct member

--- src/util.c
+++ src/util.c
@@ -245,6 +245,10 @@ static void ast_visit(node_t *n, int tab)
 		indent(tab + 1);
 		printf("%s\n", n->var->name);
 	}
+	if(n->kind == NODE_MEMBER) {
+		indent(tab + 1);
+		printf("%s\n", n->memb->ident->content);
+	}
 	if(n->kind == NODE_NUM) {
 		indent(tab + 1);
 		printf("%lld\n", n->num);
