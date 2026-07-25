563f097704ae8774a7321523c6af3b9f7b109aef
therealblue24 <99767624+therealblue24@users.noreply.github.com>
2026-07-24 23:40:43 -0400

    fixes

--- bird/src/x64.c
+++ bird/src/x64.c
@@ -446,7 +446,7 @@ static void ir_emit_blk_x64_sysv(FILE *f, ir_func_t *fn, ir_blk_t *blk,
 			}
 			p--;
 
-			for(size_t i = list_len(ins->call_args); i >= 6; i--) {
+			for(size_t i = list_len(ins->call_args) - 1; i >= 6; i--) {
 				int arg = ins->call_args[i]->r->rr;
 				if(ins->call_args[i]->r->spilld2) {
 					arg = 7;
@@ -930,7 +930,7 @@ void ir_func_emit_x64_sysv(FILE *f, ir_func_t *fun)
 			store(f, arg->size, 14, false, "[rbp - %lld]",
 				  i64abs((int64_t)arg->r->off));
 
-			stack_indx -= arg->size;
+			stack_indx -= 8;
 		}
 	}
 
